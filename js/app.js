a3e040a42abbe490026b799a0d02382ca37f69c5
Ashton K Ellis <ashtonkellis@gmail.com>
2018-04-16 16:37:04 -0700

    new items added to items array

--- js/app.js
+++ js/app.js
@@ -8,3 +8,25 @@ function Item(name, imageName, htmlId) {
   this.shownNum = 0;
 }
 
+var items = [
+  new Item('R2D2 Luggage', 'bag.jpg', 'bag'),
+  new Item('Banana Slicer', 'banana.jpg', 'banana'),
+  new Item('Bathroom iPod Stand', 'bathroom.jpg', 'bathroom'),
+  new Item('Open Toe Boots', 'boots.jpg', 'boots'),
+  new Item('All-In-One Breakfast', 'breakfast.jpg', 'breakfast'),
+  new Item('Meatball Bubble Gum', 'bubblegum.jpg', 'bubblegum'),
+  new Item('Convex Chair', 'chair.jpg', 'chair'),
+  new Item('Cthulhu Figurine', 'cthulhu.jpg', 'Cthulhu'),
+  new Item('Doggy Duck Lips', 'dog-duck.jpg', 'dog-duck'),
+  new Item('Dragon Meat', 'dragon.jpg', 'dragon'),
+  new Item('Pen Tip Utensils', 'pen.jpg', 'pen'),
+  new Item('Pet Sweep Dust Boots', 'pet-sweep.jpg', 'pet-sweep'),
+  new Item('Pizza Scissors', 'scissors.jpg', 'scissors'),
+  new Item('Sharking Bag', 'shark.jpg', 'shark'),
+  new Item('Baby Sweep Onesie', 'sweep.png', 'sweep'),
+  new Item('Tauntaun Sleeping Bag', 'tauntaun.jpg', 'tauntaun'),
+  new Item('Unicorn Mean', 'unicorn.jpg', 'unicorn'),
+  new Item('Tentacle USB', 'usb.gif', 'usb'),
+  new Item('Self-Watering Can', 'water-can.jpg', 'water-can'),
+  new Item('12-Step Wine Glass', 'wine-glass.jpg', 'wine-glass'),
+];
